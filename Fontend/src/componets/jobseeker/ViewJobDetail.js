14c1ced93e674fa8e4e81125d20b6f87dff5c9e4
toanvk <toanmodric10@gmail.com>
2024-11-13 15:07:06 +0700

    Update ViewJobDetail.js

--- Fontend/src/componets/jobseeker/ViewJobDetail.js
+++ Fontend/src/componets/jobseeker/ViewJobDetail.js
@@ -231,17 +231,52 @@ function ViewJobDetail() {
               </div>
               <div className="col-md-7 col-sm-7">
                 <div className="detail-pannel-footer-btn pull-right">
-                  <button className="button apply-button" title="Apply Now">Ứng tuyển ngay</button>
-                  {jobDetails.isWishJob ? (<button className="button save-button">
-                    <FontAwesomeIcon style={{ color: "#ff6666" }} icon={jobDetails.isSaved ? faTrash : faHeart} /> Đã Lưu
-                  </button>) : (<button onClick={toggleSaveJob} className="button save-button">
-                    <FontAwesomeIcon icon={jobDetails.isWishJob ? faTrash : faHeart} /> Lưu tin
-                  </button>)}
+                  {/* Nút Ứng tuyển ngay */}
+                  <button
+                    className="button apply-button"
+                    title="Ứng tuyển ngay"
+                    onClick={() => {
+                      const token = localStorage.getItem('token');
+                      if (!token) {
+                        navigate("/login"); // Chuyển hướng đến trang đăng nhập nếu chưa đăng nhập
+                      } else {
+                        // Thêm logic để ứng tuyển vào công việc ở đây
+                        console.log("Đang ứng tuyển vào công việc...");
+                      }
+                    }}
+                  >
+                    Ứng tuyển ngay
+                  </button>
 
+                  {/* Nút Lưu tin */}
+                  {jobDetails.isWishJob ? (
+                    <button className="button save-button">
+                      <FontAwesomeIcon style={{ color: "#ff6666" }} icon={jobDetails.isSaved ? faTrash : faHeart} /> Đã Lưu
+                    </button>
+                  ) : (
+                    <button onClick={toggleSaveJob} className="button save-button">
+                      <FontAwesomeIcon icon={jobDetails.isWishJob ? faTrash : faHeart} /> Lưu tin
+                    </button>
+                  )}
 
-                  <button onClick={() => navigate(`/reportPostJob/${id}`)} className="button report-button" title="Report">Báo cáo</button>
+                  {/* Nút Báo cáo */}
+                  <button
+                    onClick={() => {
+                      const token = localStorage.getItem('token');
+                      if (!token) {
+                        navigate("/login"); // Chuyển hướng đến trang đăng nhập nếu chưa đăng nhập
+                      } else {
+                        navigate(`/reportPostJob/${id}`);
+                      }
+                    }}
+                    className="button report-button"
+                    title="Báo cáo"
+                  >
+                    Báo cáo
+                  </button>
                 </div>
               </div>
+
             </div>
           </div>
         </div>
