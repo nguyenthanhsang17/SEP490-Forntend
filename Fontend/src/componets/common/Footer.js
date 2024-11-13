a146c0b78852110ac0c32f75fff0e29254afad8e
nguyenthanhsang17 <nguyenthanhsang17102002@gmail.com>
2024-11-13 14:50:42 +0700

    Fix footer

--- Fontend/src/componets/common/Footer.js
+++ Fontend/src/componets/common/Footer.js
@@ -22,14 +22,14 @@ class Footer extends React.Component {
       <footer className="footer">
         <div className="row no-padding">
           <div className="container">
-            <div className="col-md-3 col-sm-12">
+            <div className="col-md-8 col-sm-12">
               <div className="footer-widget">
-                <h3 className="widgettitle widget-title">Về Job Stock</h3>
+                <h3 className="widgettitle widget-title">Về VJN</h3>
                 <div className="textwidget">
-                  <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
-                  <p>7860 North Park Place<br />San Francisco, CA 94120</p>
-                  <p><strong>Email:</strong> Support@careerdesk</p>
-                  <p><strong>Gọi:</strong> <a href="tel:+15555555555">555-555-1234</a></p>
+                  <p>Nền tảng tuyển dụng và tìm việc bán thời gian trực tuyến.</p>
+                  <p>Đại học fpt, Hòa lạc</p>
+                  <p><strong>Email:</strong> quickjobvjn2024@gmail.com</p>
+                  <p><strong>Gọi:</strong> <a href="tel:+15555555555">0369354782</a></p>
                   <ul className="footer-social">
                     <li><a href="#"><i className="fa fa-facebook"></i></a></li>
                     <li><a href="#"><i className="fa fa-google-plus"></i></a></li>
@@ -41,44 +41,13 @@ class Footer extends React.Component {
               </div>
             </div>
 
-            <div className="col-md-3 col-sm-4">
-              <div className="footer-widget">
-                <h3 className="widgettitle widget-title">Tất Cả Điều Hướng</h3>
-                <div className="textwidget">
-                  <ul className="footer-navigation">
-                    <li><a href="manage-company.html" title="">Thiết kế Front-end</a></li>
-                    <li><a href="manage-company.html" title="">Lập trình Android</a></li>
-                    <li><a href="manage-company.html" title="">Phát triển CMS</a></li>
-                    <li><a href="manage-company.html" title="">Lập trình PHP</a></li>
-                    <li><a href="manage-company.html" title="">Lập trình iOS</a></li>
-                    <li><a href="manage-company.html" title="">Lập trình iPhone</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
 
-            <div className="col-md-3 col-sm-4">
-              <div className="footer-widget">
-                <h3 className="widgettitle widget-title">Tất Cả Danh Mục</h3>
-                <div className="textwidget">
-                  <ul className="footer-navigation">
-                    <li><a href="manage-company.html" title="">Thiết kế Front-end</a></li>
-                    <li><a href="manage-company.html" title="">Lập trình Android</a></li>
-                    <li><a href="manage-company.html" title="">Phát triển CMS</a></li>
-                    <li><a href="manage-company.html" title="">Lập trình PHP</a></li>
-                    <li><a href="manage-company.html" title="">Lập trình iOS</a></li>
-                    <li><a href="manage-company.html" title="">Lập trình iPhone</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-3 col-sm-4">
+            <div className="col-md-4 col-sm-4">
               <div className="footer-widget">
                 <h3 className="widgettitle widget-title">Kết Nối Với Chúng Tôi</h3>
                 <div className="textwidget">
-                  <p><strong>Điện Thoại:</strong> +1 (234) 567-8900</p>
-                  <p><strong>Email:</strong> contact@yourwebsite.com</p>
+                  <p><strong>Điện Thoại:</strong> +84 369354782</p>
+                  <p><strong>Email:</strong> quickjobvjn2024@gmail.com</p>
                 </div>
               </div>
             </div>
