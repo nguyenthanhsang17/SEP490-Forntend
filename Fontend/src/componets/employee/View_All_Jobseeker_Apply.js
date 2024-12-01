5c8ff2878f0fc856421926836cb5b59144cb85c3
toanvk <toanmodric10@gmail.com>
2024-12-01 22:17:10 +0700

    Update View_All_Jobseeker_Apply.js

--- Fontend/src/componets/employee/View_All_Jobseeker_Apply.js
+++ Fontend/src/componets/employee/View_All_Jobseeker_Apply.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import "../assets/css/style.css";
 import '../assets/plugins/css/plugins.css';
 import '../assets/css/colors/green-style.css';
+import bannerImage from '../assets/img/banner-10.jpg';
 import Footer from '../common/Footer';
 import Header from '../common/Header';
 import axios from 'axios';
@@ -62,79 +63,96 @@ function ViewAllJobSeekerApply() {
   return (
     <>
       <Header />
+      <section className="inner-header-title" style={{ backgroundImage: `url(${bannerImage})` }}>
+        <div className="container">
+          <h1 className="text-center">Danh sách ứng viên</h1>
+        </div>
+      </section>
       <div className="container job-seeker-list" style={{ paddingTop: '100px' }}>
-        <h2 className="text-center">Danh sách ứng viên</h2>
-
         {/* Ô tìm kiếm */}
-        <div className="mb-4" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <div className="form-group" style={{ flex: '1', marginRight: '10px' }}>
-            <label htmlFor="genderFilter">Giới tính:</label>
-            <select
-              id="genderFilter"
-              className="form-control"
-              value={genderFilter}
-              onChange={e => setGenderFilter(e.target.value)}
-            >
-              <option value="">Tất cả</option>
-              <option value="Nam">Nam</option>
-              <option value="Nữ">Nữ</option>
-            </select>
-          </div>
-          <div className="form-group" style={{ flex: '1', marginRight: '10px' }}>
-            <label htmlFor="ageFilter">Tuổi từ:</label>
-            <input
-              type="number"
-              id="ageFilter"
-              className="form-control"
-              value={ageFilter}
-              onChange={e => setAgeFilter(e.target.value)}
-              placeholder="Nhập tuổi"
-            />
-          </div>
+        <div
+          className="filter-section"
+          style={{
+            backgroundColor: '#f9fbfc',
+            borderRadius: '8px',
+            boxShadow: '0 4px 15px rgba(0, 0, 0, 0.1)',
+            padding: '20px',
+            marginBottom: '20px',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '15px',
+          }}
+        >
+          <div className="mb-4" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <div className="form-group" style={{ flex: '1', marginRight: '10px' }}>
+              <label htmlFor="genderFilter">Giới tính:</label>
+              <select
+                id="genderFilter"
+                className="form-control"
+                value={genderFilter}
+                onChange={e => setGenderFilter(e.target.value)}
+              >
+                <option value="">Tất cả</option>
+                <option value="Nam">Nam</option>
+                <option value="Nữ">Nữ</option>
+              </select>
+            </div>
+            <div className="form-group" style={{ flex: '1', marginRight: '10px' }}>
+              <label htmlFor="ageFilter">Tuổi từ:</label>
+              <input
+                type="number"
+                id="ageFilter"
+                className="form-control"
+                value={ageFilter}
+                onChange={e => setAgeFilter(e.target.value)}
+                placeholder="Nhập tuổi"
+              />
+            </div>
 
-          <div className="form-group" style={{ flex: '1', marginRight: '10px' }}>
-            <label htmlFor="ageFilter2">Tuổi đến:</label>
-            <input
-              type="number"
-              id="ageFilter2"
-              className="form-control"
-              value={ageFilter2}
-              onChange={e => setAgeFilter2(e.target.value)}
-              placeholder="Nhập tuổi"
-            />
-          </div>
+            <div className="form-group" style={{ flex: '1', marginRight: '10px' }}>
+              <label htmlFor="ageFilter2">Tuổi đến:</label>
+              <input
+                type="number"
+                id="ageFilter2"
+                className="form-control"
+                value={ageFilter2}
+                onChange={e => setAgeFilter2(e.target.value)}
+                placeholder="Nhập tuổi"
+              />
+            </div>
 
-          <div className="form-group" style={{ flex: '1', marginRight: '10px' }}>
-            <label htmlFor="jobFilter">Công việc hiện tại:</label>
-            <select
-              id="jobFilter"
-              className="form-control"
-              value={jobFilter}
-              onChange={e => setJobFilter(e.target.value)}
-            >
-              <option value="">Tất cả</option>
-              <option value="Đang đi học">Đang đi học</option>
-              <option value="Đang đi làm">Đang đi làm</option>
-              <option value="Thất nghiệp">Thất nghiệp</option>
-            </select>
+            <div className="form-group" style={{ flex: '1', marginRight: '10px' }}>
+              <label htmlFor="jobFilter">Công việc hiện tại:</label>
+              <select
+                id="jobFilter"
+                className="form-control"
+                value={jobFilter}
+                onChange={e => setJobFilter(e.target.value)}
+              >
+                <option value="">Tất cả</option>
+                <option value="Đang đi học">Đang đi học</option>
+                <option value="Đang đi làm">Đang đi làm</option>
+                <option value="Thất nghiệp">Thất nghiệp</option>
+              </select>
+            </div>
+            <div className="form-group" style={{ flex: '1', marginRight: '10px' }}>
+              <label htmlFor="applyFilter">Trạng thái ứng tuyển:</label>
+              <select
+                id="applyFilter"
+                className="form-control"
+                value={applyFilter}
+                onChange={e => setApplyFilter(e.target.value)}
+              >
+                <option value="">Tất cả</option>
+                <option value="0">Đang chờ</option>
+                <option value="1">Không phù hợp</option>
+                <option value="3">Đã xem thông tin liên lạc</option>
+                <option value="4">Đã nhận</option>
+                <option value="5">Không nhận</option>
+              </select>
+            </div>
+            <button onClick={handleSearch} className="btn btn-primary">Tìm kiếm</button>
           </div>
-          <div className="form-group" style={{ flex: '1', marginRight: '10px' }}>
-            <label htmlFor="applyFilter">Trạng thái ứng tuyển:</label>
-            <select
-              id="applyFilter"
-              className="form-control"
-              value={applyFilter}
-              onChange={e => setApplyFilter(e.target.value)}
-            >
-              <option value="">Tất cả</option>
-              <option value="0">Đang chờ</option>
-              <option value="1">Không phù hợp</option>
-              <option value="3">Đã xem thông tin liên lạc</option>
-              <option value="4">Đã nhận</option>
-              <option value="5">Không nhận</option>
-            </select>
-          </div>
-          <button onClick={handleSearch} className="btn btn-primary">Tìm kiếm</button>
         </div>
 
         <div className="row justify-content-center">
